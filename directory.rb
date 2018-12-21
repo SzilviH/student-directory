f6a1bf792ae5d74abf4d48ab831215b4c244de1a
Szilvi Horvath <szil.horvath@gmail.com>
2018-12-21 13:35:19 +0000

    refactoring code

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
-# let's put all students into an array
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create and empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -16,41 +15,48 @@ def input_students
       cohort = "november"
     end
     # add the student and their cohort hash to the array
-    students << {name: name, cohort: cohort.to_sym} # cohort string is converted to symbol
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort.to_sym} # cohort string is converted to symbol
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
 
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit # this will cause the program to terminate
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you mean, try again"
   end
 end
 
@@ -59,17 +65,17 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_by_cohort(students)
-  cohorts = students.map {|student| student[:cohort]}.uniq
+  cohorts = @students.map {|student| student[:cohort]}.uniq
   cohorts.each do |cohort|
     puts "#{cohort} cohort: "
-    students.each do |student|
+    @students.each do |student|
       if student[:cohort] == cohort
         puts student[:name]
       end
@@ -77,17 +83,12 @@ def print_by_cohort(students)
   end
 end
 
-def print_footer(names)
-  if names.count > 1
-    puts "Overall, we have #{names.count} great students"
+def print_footer
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students"
   else
-    puts "Overall, we have #{names.count} great student"
+    puts "Overall, we have #{@students.count} great student"
   end
 end
 
 interactive_menu
-# Nothing happens until we call the methods
-print_header
-#print(students)
-print_by_cohort(students)
-print_footer(students)
